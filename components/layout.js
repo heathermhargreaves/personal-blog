c14ad94f1976b73544c700caf3fa303bd8e42955
heathermhargreaves <heathermhargreaves@gmail.com>
2024-06-01 00:15:55 -0700

    adding vercel analytics

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
+
 const name = 'Heather Hargreaves';
 export const siteTitle = 'professional blog';
 
